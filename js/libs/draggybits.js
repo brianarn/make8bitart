9034917d3768bb6feeaf058a37dba28dc9b55a6a
Brian Arnold <brianarn@gmail.com>
2015-04-07 22:37:43 -0600

    Add keyboard support to draggyBits, display with a highlight

--- js/libs/draggybits.js
+++ js/libs/draggybits.js
@@ -11,7 +11,7 @@
   var zIndex = 100;
   var pos = { x:0, y:0 };
   var numDraggers = 0;
-  var tileOffset = { x:20, y:20};
+  var tileOffset = { x:20, y:20 };
 
   var $current;
   var $window = $(window);
@@ -21,6 +21,22 @@
     onInit : function (e) { return false; },
     onClose : function (e) { return false; },
     onRestore : function (e) { return false; }
+  };
+
+  // We aren't using *all* of these keycodes yet, but they sure are handy
+  var keyCodes = {
+    DOWN : 40,
+    END : 35,
+    ENTER : 13,
+    ESCAPE : 27,
+    HOME : 36,
+    LEFT : 37,
+    PAGE_DOWN : 34,
+    PAGE_UP : 33,
+    RIGHT : 39,
+    SPACE : 32,
+    TAB : 9,
+    UP : 38
   };
 
   var methods = {
@@ -50,6 +66,12 @@
         $this.data(pluginName, data);
 
         $dragger.attr('aria-grabbed', false);
+
+        // If there isn't a tabindex, set one on the dragger in order to ensure
+        // focusability, even for not-normally focusable elements
+        if (!$dragger.attr('tabindex')) {
+          $dragger.attr('tabindex', '0');
+        }
 
         numDraggers++;
 
@@ -125,7 +147,7 @@
     }
 
     $this.attr('aria-grabbed', false);
-    $('.' + movingClass).removeClass(movingClass)
+    $('.' + movingClass).removeClass(movingClass);
 
     $window.off('mousemove');
     isMoving = false;
@@ -144,7 +166,7 @@
       return;
     }
 
-    e.preventDefault()
+    e.preventDefault();
     pos = { x: e.pageX, y: e.pageY };
 
     $this.attr('aria-grabbed', true);
@@ -163,6 +185,93 @@
       $par[pluginName]('close');
   };
 
+  // Some of this logic is duplicative of onMouseDown/onMouseUp,
+  // but given that we don't have pageX/pageY, reuse of the other
+  // events proves difficult.
+  var onKeyDown = function (e) {
+    var $this = $(e.target);
+    var isDragger = $this.hasClass(draggerClass);
+
+    // How much to move when hitting various directions
+    // Shift causes large movements, Control causes small ones
+    // Control+Shift is treated like Control
+    // lol alt/cmd
+    var moveAmount = 10;
+    if (e.shiftKey) { moveAmount = 100; }
+    if (e.ctrlKey) { moveAmount = 1; }
+
+    var movePos;
+
+    // Only care about key events on draggers
+    if (!isDragger) {
+      return;
+    }
+
+    $current = $this.parents('.'+ pluginName);
+
+    switch (e.keyCode) {
+      // Intentional fall-through so that both spacebar and enter
+      // act as activation keys
+      case keyCodes.SPACE:
+      case keyCodes.ENTER:
+        e.preventDefault();
+
+        $current.css("z-index", zIndex++);
+
+        isMoving = !isMoving;
+        $this.attr('aria-grabbed', isMoving);
+        $current.toggleClass(movingClass, isMoving);
+        break;
+
+      case keyCodes.TAB:
+        // Tabbing will automatically let go of the control and move on
+        isMoving = false;
+        $this.attr('aria-grabbed', isMoving);
+        $current.removeClass(movingClass);
+        break;
+
+      // For various up/down/left/right maneuvers, only move when grabbed.
+      // Lots of duplicated code, but hard to clean up in a way that is still
+      // readable/maintainable.
+      case keyCodes.UP:
+        if (!isMoving) { return; }
+        e.preventDefault();
+        movePos = { top : "-=" + moveAmount };
+        break;
+      case keyCodes.DOWN:
+        if (!isMoving) { return; }
+        e.preventDefault();
+        movePos = { top : "+=" + moveAmount };
+        break;
+      case keyCodes.LEFT:
+        if (!isMoving) { return; }
+        e.preventDefault();
+        movePos = { left : "-=" + moveAmount };
+        break;
+      case keyCodes.RIGHT:
+        if (!isMoving) { return; }
+        e.preventDefault();
+        movePos = { left : "+=" + moveAmount };
+        break;
+
+      default:
+        // We obvs don't care about this key, get out
+        return;
+    }
+
+    // If we have a move value, apply it now
+    if (movePos) {
+      // Move as appropriate
+      $current.css(movePos);
+
+      // If our top or left went negative, move back to 0,
+      // to keep us from going off-screen too hard.
+      if (parseInt($current.css('top'), 10) < 0)  { $current.css('top', '0');  }
+      if (parseInt($current.css('left'), 10) < 0) { $current.css('left', '0'); }
+    }
+
+  };
+
   var onMinimizeClick = function (e) {
     var $this = $(this);
     var $par = $this.parents('.'+ pluginName);
@@ -176,5 +285,6 @@
   // touch
   window.addEventListener('touchstart', onMouseDown, false);
   window.addEventListener('touchend', onMouseUp, false);
+  window.addEventListener('keydown', onKeyDown, false);
 
 })( jQuery );
